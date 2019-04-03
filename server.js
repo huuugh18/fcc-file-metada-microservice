b5269b832f5f4595f266b783a1a89bcee0eafb14
Hugh O'Neill <hugh.oneill.4@gmail.com>
2019-04-03 09:43:26 -0600

    init commit

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 
 var express = require('express');
 var cors = require('cors');
-
+var multer = require('multer');
 // require and use "multer"...
+var upload = multer({ dest: 'uploads/' })
+
 
 var app = express();
 
@@ -13,6 +15,11 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res,next) {
+  const data = req.file;
+  res.json({name:data.originalname,type:data.mimetype,size:data.size});
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
